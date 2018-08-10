4e18eeb93f28494358d248748c3bcda1c14d7960
Xinwei Chen <chenxinwei1234@gmail.com>
2018-08-10 15:57:26 +0200

    04

--- app/src/main/java/com/example/xinweichen/tetris2048/GameView.java
+++ app/src/main/java/com/example/xinweichen/tetris2048/GameView.java
@@ -5,22 +5,31 @@ import android.util.AttributeSet;
 import android.widget.GridLayout;
 
 public class GameView extends GridLayout {
-    public GameView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
 
+
+    public GameView(Context context) {
+        super(context);
+        initGameView();
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initGameView();
     }
 
     public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initGameView();
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initGameView();
     }
 
 
-    public GameView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+   private void initGameView(){
 
-    public GameView(Context context) {
-        super(context);
-    }
+   }
 
 }
